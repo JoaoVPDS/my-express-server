01e9376201a668c23eb51a441b4fb33e3d292c07
JoaoVPDS <92056212+JoaoVPDS@users.noreply.github.com>
2022-09-09 17:51:06 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.listen(8080, function(){
+app.get('/', function (req, res) {
+    res.send("<h1>Hello, world!</h1>")
+})
+
+app.get('/contact', function (req, res) {
+    res.send("Contact me at: joao.vpds07@gmail.com");
+})
+
+app.get('/about', function (req, res) {
+    res.send("<h1>This is a page about me (:</h1><br>My name is Joao Vinicius, I'm a high school student that study system develpment at IFAL, I started working at OG1 yesterday. They asked me to learn NODE, especially express because they want to creant an API to use in the future, so here am I.")
+})
+
+app.listen(8080, function () {
     console.log("Server started on port 8080");
 });
